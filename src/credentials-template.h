d56c53bdbfe82448d176afda83d8dd266f6e11cc
vesatam <36679709+vesatam@users.noreply.github.com>
2023-02-26 18:09:03 +0200

    Update credentials-template.h

--- src/credentials-template.h
+++ src/credentials-template.h
@@ -1,8 +1,8 @@
 // WiFi credentials
-const char *ssid = "FreeWifi";
-const char *password = "hardtoguess";
+const char *ssid = "vesat";
+const char *password = "Marja-Anneli";
 
 // Husarnet credentials
-const char *hostName = "my-esp32";
-const char *husarnetJoinCode = "fc94:b01d:1803:8dd8:b293:5c7d:7639:932a/xhfqwPxxxetyCExsSPRPn9"; // find at app.husarnet.com
+const char *hostName = "ESP32_Vesa";
+const char *husarnetJoinCode = "fc94:b01d:1803:8dd8:b293:5c7d:7639:932a/izUnuh6oAeJ9miwACuLWby"; // find at app.husarnet.com
 const char *dashboardURL = "default";   // in you use Husarnet self-hosted Dashboard you can specify URL here. Otherwise keep "default"
